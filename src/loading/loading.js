9271486039c2817d4358b1ba1be6913638dc1075
widdy <kingwide@foxmail.com>
2020-03-25 23:13:50 +0800

    toast & loading

--- src/loading/loading.js
+++ src/loading/loading.js
@@ -1,13 +1,13 @@
 /*
 * Tencent is pleased to support the open source community by making WeUI.js available.
-* 
+*
 * Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
-* 
+*
 * Licensed under the MIT License (the "License"); you may not use this file except in compliance
 * with the License. You may obtain a copy of the License at
-* 
+*
 *       http://opensource.org/licenses/MIT
-* 
+*
 * Unless required by applicable law or agreed to in writing, software distributed under the License is
 * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
 * either express or implied. See the License for the specific language governing permissions and
@@ -19,36 +19,36 @@ import tpl from './loading.html';
 
 let _sington;
 
-/**
- * loading
- * @param {string} content loading的文字
- * @param {object=} options 配置项
- * @param {string=} options.className 自定义类名
- *
- * @example
- * var loading = weui.loading('loading', {
- *     className: 'custom-classname'
- * });
- * setTimeout(function () {
- *     loading.hide(function() {
- *          console.log('`loading` has been hidden');
- *      });
- * }, 3000);
- */
-function loading(content = '', options = {}) {
-    if(_sington) return _sington;
+export function showLoading (object = {}) {
+    if (_sington) return _sington;
 
-    options = $.extend({
-        content: content,
-        className: ''
-    }, options);
+    object = $.extend({
+        title: '',
+        className: '',
+        success: $.noop
+    }, object);
 
-    const $loadingWrap = $($.render(tpl, options));
+    const $loadingWrap = $($.render(tpl, object));
     const $loading = $loadingWrap.find('.weui-toast');
     const $mask = $loadingWrap.find('.weui-mask');
 
-    function _hide(callback) {
-        _hide = $.noop; // 防止二次调用导致报错
+    $('body').append($loadingWrap);
+    $loading.addClass('weui-animate-fade-in');
+    $mask.addClass('weui-animate-fade-in');
+    object.success();
+    _sington = $loadingWrap[0];
+    return _sington;
+}
+
+export function hideLoading (object = {}) {
+    object = $.extend({
+        success: $.noop
+    }, object);
+
+    if (_sington) {
+        const $loadingWrap = $(_sington);
+        const $loading = $loadingWrap.find('.weui-toast');
+        const $mask = $loadingWrap.find('.weui-mask');
 
         $mask.addClass('weui-animate-fade-out');
         $loading
@@ -56,17 +56,7 @@ function loading(content = '', options = {}) {
             .on('animationend webkitAnimationEnd', function () {
                 $loadingWrap.remove();
                 _sington = false;
-                callback && callback();
+                object.success();
             });
     }
-    function hide(callback){ _hide(callback); }
-
-    $('body').append($loadingWrap);
-    $loading.addClass('weui-animate-fade-in');
-    $mask.addClass('weui-animate-fade-in');
-
-    _sington = $loadingWrap[0];
-    _sington.hide = hide;
-    return _sington;
 }
-export default loading;
--- src/toast/toast.js
+++ src/toast/toast.js
@@ -1,13 +1,13 @@
 /*
 * Tencent is pleased to support the open source community by making WeUI.js available.
-* 
+*
 * Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
-* 
+*
 * Licensed under the MIT License (the "License"); you may not use this file except in compliance
 * with the License. You may obtain a copy of the License at
-* 
+*
 *       http://opensource.org/licenses/MIT
-* 
+*
 * Unless required by applicable law or agreed to in writing, software distributed under the License is
 * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
 * either express or implied. See the License for the specific language governing permissions and
@@ -19,44 +19,22 @@ import tpl from './toast.html';
 
 let _sington;
 
-/**
- * toast 一般用于操作成功时的提示场景
- * @param {string} content toast的文字
- * @param {Object|function=} options 配置项或回调
- * @param {number=} [options.duration=3000] 多少毫秒后关闭toast
- * @param {function=} options.callback 关闭后的回调
- * @param {string=} options.className 自定义类名
- *
- * @example
- * weui.toast('操作成功', 3000);
- * weui.toast('操作成功', {
- *     duration: 3000,
- *     className: 'custom-classname',
- *     callback: function(){ console.log('close') }
- * });
- */
-function toast(content = '', options = {}) {
-    if(_sington) return _sington;
+export function showToast (object = {}) {
+    if (_sington) return _sington;
 
-    if (typeof options === 'number') {
-        options = {
-            duration: options
-        };
-    }
-    if (typeof options === 'function') {
-        options = {
-            callback: options
-        };
-    }
-
-    options = $.extend({
-        content: content,
-        duration: 3000,
-        callback: $.noop,
+    object = $.extend({
+        title: '',
+        icon: 'success', // none,loading
+        duration: 1500,
+        success: $.noop,
+        fail: $.noop,
+        complete: $.noop,
         className: ''
-    }, options);
+    }, object);
 
-    const $toastWrap = $($.render(tpl, options));
+    object.icon = {success: 'weui-icon-success-no-circle', loading: 'weui-loading', none: ''}[object.icon] || '';
+
+    const $toastWrap = $($.render(tpl, object));
     const $toast = $toastWrap.find('.weui-toast');
     const $mask = $toastWrap.find('.weui-mask');
 
@@ -71,11 +49,33 @@ function toast(content = '', options = {}) {
             .on('animationend webkitAnimationEnd', function () {
                 $toastWrap.remove();
                 _sington = false;
-                options.callback();
+                object.success();
             });
-    }, options.duration);
+    }, object.duration);
 
     _sington = $toastWrap[0];
+
     return $toastWrap[0];
 }
-export default toast;
+
+export function hideToast (object = {}) {
+    object = $.extend({
+        success: $.noop
+    }, object);
+
+    if (_sington) {
+        const $toastWrap = $(_sington);
+        const $toast = $toastWrap.find('.weui-toast');
+        const $mask = $toastWrap.find('.weui-mask');
+
+        $mask.addClass('weui-animate-fade-out');
+        $mask.addClass('weui-animate-fade-out');
+        $toast
+            .addClass('weui-animate-fade-out')
+            .on('animationend webkitAnimationEnd', function () {
+                $toastWrap.remove();
+                _sington = false;
+                object.success();
+            });
+    }
+}
